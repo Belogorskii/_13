6fb014129275ab360236bb3f22ae997c8da860bb
Kirill Miller <belogorski2000@gmail.com>
2022-08-27 12:27:01 +0300

    Добавьте файлы проекта.

---
+++ _13/MultiBuilding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    sealed class MultiBuilding : Bilding
+    {
+        string floors { get; set; }
+
+        public MultiBuilding(string buildingAddress, string buildingLongh, string buildingWidth, string buildingHeight, string floors)
+         : base(buildingAddress, buildingLongh, buildingWidth, buildingHeight)
+        {
+            this.floors = floors;
+        }
+        public string Print()
+        {
+            string result = base.Print();
+            result += $"{floors}";
+            return result;
+        }
+    }
+}
---
+++ _13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bilding bilding = new Bilding("jhf", "vhjjv", "klnn", "kbiio");
+            Console.WriteLine(bilding.Print());
+            MultiBuilding multiBuilding = new MultiBuilding("qfqf", "fqqfq", "adaa", "aafaf", "acac");
+            Console.WriteLine(multiBuilding.Print());
+            Console.ReadKey();
+        }
+    }
+}
